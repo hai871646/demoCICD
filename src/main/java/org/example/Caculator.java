7bac579b2e8a57098fc99ac0a8646c731df9a524
Huỳnh Trung Hải <hai871686@gmail.com>
2025-04-09 12:15:19 +0700

    first commit

---
+++ src/main/java/org/example/Caculator.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Caculator  {
+    Integer sum(Integer a, Integer b) {
+        return 0;
+    }
+
+    Integer sub(Integer a, Integer b) {
+        return 0;
+    }
+}
---
+++ src/main/java/org/example/ICaculator.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class ICaculator {
+    Integer sum(Integer a, Integer b) {
+        return null;
+    }
+
+    Integer sub(Integer a, Integer b) {
+        return null;
+    }
+}
